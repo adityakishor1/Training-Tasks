add41f43fd1161a53bc5aa8f7258c848d0ef90d6
adityakishor1 <adikishor67@gmail.com>
2025-06-20 15:36:54 +0530

    new work

---
+++ Javascript/equal.js
@@ -0,0 +1,27 @@
+
+let a = 5;
+let b = '5';
+
+console.log("a == b:", a == b);   
+console.log("a === b:", a === b); 
+
+let x = 0;
+let y = false;
+
+console.log("x == y:", x == y);  
+console.log("x === y:", x === y);
+
+
+let val1 = null;
+let val2 = undefined;
+
+console.log("val1 == val2:", val1 == val2);   
+console.log("val1 === val2:", val1 === val2); 
+
+
+
+let str = "true";
+let bool = true;
+
+console.log("str == bool:", str == bool);   
+console.log("str === bool:", str === bool); 
