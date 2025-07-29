20541d4f214660e608c5232809593a41d853bfe1
adityakishor1 <adikishor67@gmail.com>
2025-07-29 17:15:32 +0530

    new push

---
+++ Java Practise/zeros.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class zeros {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0};
+        zeros(arr);
+        System.out.println(Arrays.toString(arr)); 
+    }
+
+    public static void zeros(int[] arr) {
+        int n = arr.length;
+        int index = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] != 0) {
+                arr[index--] = arr[i];
+            }
+        }
+        while (index >= 0) {
+            arr[index--] = 0;
+        }
+    }
+}
